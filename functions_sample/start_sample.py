1c4ae2f70f493d5f167e58a62311495454c50a25
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2015-11-28 15:40:11 -0500

    corrected module path bug

--- functions_sample/start_sample.py
+++ functions_sample/start_sample.py
@@ -1,6 +1,6 @@
 __author__ = 'juan pablo isaza'
 
-from functions_sample.functions1 import *
+from functions1 import *
 
 and_result = and_function(True, True)  # should be: 'True and True = True', after running test1.
 or_result = or_function(True, False)  # should be: 'True or False = True', after running test1.
