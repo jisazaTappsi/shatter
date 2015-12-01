204a1454c5efd1234a1ab23bc3ce63095663a150
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2015-12-01 00:41:56 -0500

    added pypandoc to have style on pypi and on github with only 1 readme

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ __author__ = 'juan pablo isaza'
 
 from setuptools import setup
 import setuptools
+import pypandoc
 
 setup(
     name='Boolean Solver',
-    version='0.1.10',
+    version='0.1.11',
     author='Juan Pablo Isaza',
     author_email='biosolardecolombia@gmail.com',
     description='Fast development with generated boolean expressions.',
-    long_description=open('README.md').read(),
+    long_description=pypandoc.convert('README.md', 'rst'),
     license='MIT',
     keywords='Quine McCluskey, Boolean, code, automatic code generation, expression, Boolean expression',
     url='https://github.com/jisazaTappsi/BooleanSolver',
