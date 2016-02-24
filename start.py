7710e022a5d1996a7c8e18d0ba2ecf73672bc3d5
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2016-02-24 01:22:45 -0500

    readme changes and added readme files

---
+++ start.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Short example"""
+
+__author__ = 'juan pablo isaza'
+
+from boolean_solver import solver as s
+
+
+@s.solve()
+def and_function(a, b):
+    pass
+
+
+@s.solve()
+def if_function(a, b):
+    pass
+
+
+@s.solve()
+def recursive(a):
+    pass
+
+
+@s.solve()
+def internal_code(a):
+    pass
