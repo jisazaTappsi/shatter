43bc3fbc249d39062e5301de9cf9978d50378bf8
juan pablo <jisaza@tappsi.co>
2016-09-19 00:33:27 -0500

    complete pre-integration for awesome new feature

--- boolean_solver/code.py
+++ boolean_solver/code.py
@@ -27,5 +27,35 @@ class Code:
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.code_str == other.code_str
 
+    def get_use_case(self):
+        """0 for operands, 1 for code as string and 2 for NotImplemented"""
+        if self.rho is not None and self.operator is not None and self.lho is not None and self.code_str is None:
+            return 0
+        elif self.rho is None and self.operator is None and self.lho is None and self.code_str is not None:
+            return 1
+        else:
+            return 2
+
+    def get_operands_str(self, operand):
+        """had to try-except in order to not import MagicVar, because ir would imply a circular reference, as Code is
+        imported in MagicVar class.
+        :param operand: Can be MagicVar or any other stuff, that is operated on.
+        :returns string of that operand, ie enhanced str(operand)"""
+
+        try:
+            return operand.get_variable_name(self.the_locals)
+        except AttributeError:
+            return str(operand)
+
     def __str__(self):
-        return self.code_str
+        """3 possible behaviors: 0 awesome code with MagicVar. 1 standard code as string. 2 raise error"""
+        case = self.get_use_case()
+
+        if case == 0:
+            return '{rho} {operator} {lho}'.format(rho=self.get_operands_str(self.rho),
+                                                   operator=self.operator.symbol,
+                                                   lho=self.get_operands_str(self.lho))
+        elif case == 1:
+            return self.code_str
+        else:
+            raise NotImplemented
