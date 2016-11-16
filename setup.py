92e358dd627174ef78aabd6fc66559128efa9ba5
juan pablo <jisaza@tappsi.co>
2016-11-16 01:25:04 -0500

    changes python version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.6',
 		'Intended Audience :: Developers',
 		'Topic :: Software Development :: Build Tools',
 	],
